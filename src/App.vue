3d98bffc2201efb50b3703ce9970342157b61bec
Sergio Klein <sdkd17@gmail.com>
2022-04-05 11:07:13 -0300

    underline menu animation

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@
         
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav class="mr-auto">
-            <b-nav-item to="/"> Home </b-nav-item>
-            <b-nav-item to="/about">About</b-nav-item>
-            <b-nav-item to="/contact">Contact</b-nav-item>
+          <b-navbar-nav class="mr-auto my-animation">
+            <b-nav-item to="/"> <span> Home </span> </b-nav-item>
+            <b-nav-item to="/about"> <span> About </span> </b-nav-item>
+            <b-nav-item to="/contact"> <span> Contact </span> </b-nav-item>
           </b-navbar-nav>
 
           <b-navbar-brand class="mx-auto" href="#" >NavBar</b-navbar-brand>
