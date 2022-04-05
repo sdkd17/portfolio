b053f825034937a1816f8bfe4d14ed35ba5989d3
Sergio Klein <sdkd17@gmail.com>
2022-04-05 11:57:08 -0300

    start about page

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav class="mr-auto my-animation">
+          <b-navbar-nav class="mr-auto my-underline">
             <b-nav-item to="/"> <span> Home </span> </b-nav-item>
             <b-nav-item to="/about"> <span> About </span> </b-nav-item>
             <b-nav-item to="/contact"> <span> Contact </span> </b-nav-item>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <b-container>
+    <b-row align-v="center">
+      <b-col>
+        <b-img :src="require('../assets/papa.jpg')" fluid alt="profile"> </b-img>           
+      </b-col>
+      <b-col class="px-5 about-me">
+        <h2 class="mb-4">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
