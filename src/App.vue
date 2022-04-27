870b79405a3b0ba7c5aaf961c02397815ba150c1
Sergio Klein <sdkd17@gmail.com>
2022-04-27 11:58:07 -0300

    navbar collapse

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
   
-    <b-navbar sticky type="light" toogable="lg" class="py-3 myNav">
+    <b-navbar sticky type="light" toggleable="lg" class="py-3 myNav">
       
+      <b-navbar-brand class="mr-auto nav-name d-lg-none" to="/" >Sergio Klein</b-navbar-brand>
+
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
       <b-collapse id="nav-collapse" is-nav>
+
         <b-navbar-nav class="mr-auto my-underline">
           <b-nav-item to="/"> <span> Home </span> </b-nav-item>
           <b-nav-item to="/about"> <span> About </span> </b-nav-item>
           <b-nav-item to="/contact"> <span> Contact </span> </b-nav-item>
         </b-navbar-nav>
 
-        <b-navbar-brand class="mx-auto nav-name" to="/" >Sergio Klein</b-navbar-brand>
+        <b-navbar-brand class="mx-auto nav-name d-none d-lg-block" to="/" >Sergio Klein</b-navbar-brand>
 
         <b-navbar-nav class="ml-auto"> 
           <b-nav-item href="https://www.instagram.com/gersiok/" target="_blank"> 
